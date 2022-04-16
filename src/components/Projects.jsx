867b26f833226a777cea16b5383a17398e1d8415
mmitusov <maksym.mitusov@gmail.com>
2022-04-16 09:28:54 +0300

    CSS Testing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
 			<button className='flex-auto w-20 h-10 m-1 bg-blue-900 text-white hover:bg-green-700 hover:rounded-xl duration-300'>Hello!</button>
 			<button className='flex-auto w-20 h-10 m-1 bg-blue-900 text-white hover:bg-green-700 hover:rounded-xl duration-300'>Holla!</button>
 			<button className='flex-auto w-20 h-10 m-1 bg-blue-900 text-white hover:bg-green-700 hover:rounded-xl duration-300'>Bonjure!</button>
-			<button className='flex-auto w-20 h-10 m-1 bg-blue-900 text-white hover:bg-green-700 hover:rounded-xl duration-300'>Nihao!</button>
+			<button className='flex-auto w-20 h-10 m-1 bg-blue-900 text-white hover:bg-green-700 hover:rounded-xl duration-300'>Nihaooo!</button>
 		</div>
 	);
 }
